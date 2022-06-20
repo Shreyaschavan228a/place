3b3aacafd5cd4c27edfa07154f997fecad4ed126
Shreyas Chavan <shreyaschavan228a@gmail.com>
2022-06-20 18:32:50 +0530

    base app skeleton

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="h-16 bg-black text-white p-2 w-full">
+            <h1 className="m-auto text-4xl pl-6 ">Place</h1>
+        </div>
+    )
+}
+
+export default Header;
---
+++ components/MainContainer.jsx
@@ -0,0 +1,9 @@
+const MainContainer = () => {
+    return (
+        <div className="w-full grow bg-slate-50">
+
+        </div>
+    )
+}
+
+export default MainContainer;
