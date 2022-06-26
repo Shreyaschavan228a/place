8655f75902c5353ba3a9a9ec399ae606062c2b4a
Shreyas Chavan <shreyaschavan228a@gmail.com>
2022-06-26 19:10:56 +0530

    canvasHelper.ts

--- lib/canvasHelper.ts
+++ lib/canvasHelper.ts
@@ -1,19 +1,24 @@
-const CanvasHelper = () => {
-    let _canvasContext = null;
-    const _pixelSize = 25;
-    let _pixelData = null;
+interface coordinate {
+    x: number,
+    y: number
+}
 
+const CanvasHelper = () => {
+    const _pixelSize = 25;
+    let _canvasContext : CanvasRenderingContext2D;
+    let _pixelData;
+    let _canvasElem : HTMLCanvasElement;
     const _colors = {
-        violet : "#7c3aed",
-        indigo : "#4f46ef",
-        blue : "#2563eb",
-        green : "#16a34a",
-        yellow : "#ca8a04",
-        orange : "#ea580c",
-        red : "#dc2626"
+            violet : "#7c3aed",
+            indigo : "#4f46ef",
+            blue : "#2563eb",
+            green : "#16a34a",
+            yellow : "#ca8a04",
+            orange : "#ea580c",
+            red : "#dc2626"
     }
-    let _canvasElem = null;
-    const _getMousePos = (clickEvent) => {
+
+    const _getMousePos = (clickEvent : MouseEvent) : coordinate => {
         const rect = _canvasElem.getBoundingClientRect();
         return {
             x: clickEvent.clientX - rect.left,
@@ -21,8 +26,8 @@ const CanvasHelper = () => {
         }
     }
 
-    const initCanvas = (canvasElemRef, width, height) => {
-        _canvasContext = canvasElemRef.getContext('2d');
+    const initCanvas = (canvasElemRef : HTMLCanvasElement, width : number, height : number) => {
+        _canvasContext = canvasElemRef.getContext("2d")!;
         _canvasContext.fillStyle = "#000";
         _canvasContext.fillRect(0, 0, width, height);
         _canvasElem = canvasElemRef;
@@ -32,20 +37,20 @@ const CanvasHelper = () => {
             let tmpArr = new Array(width/_pixelSize).fill(0);
             _pixelData[i] = tmpArr;
         }
-    }
+   }
 
-    const colorBlock = (clickEvent, colorName) => {
-        const {x, y} = _getMousePos(clickEvent);
+   const colorBlock = (clickEvent : MouseEvent, colorName : string) => {
+        let {x, y} = _getMousePos(clickEvent);
         [x,y] = [Math.floor(x/_pixelSize)*_pixelSize, Math.floor(y/_pixelSize)*_pixelSize];
         _canvasContext.fillStyle = _colors[colorName];
         _canvasContext.fillRect(x, y, _pixelSize, _pixelSize);
 
         _pixelData[x/_pixelSize][y/_pixelSize] = colorName;
-    }
+   }
 
-    return {initCanvas, colorBlock};
+   return {initCanvas, colorBlock};
 }
 
 const canvasHelper = CanvasHelper();
 
-export default canvasHelper;
+export default canvasHelper; 
