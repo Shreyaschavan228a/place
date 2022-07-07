b9329a2820a376d11325b5fe28106d61d3eda7a9
Shreyas Chavan <shreyaschavan228a@gmail.com>
2022-07-07 18:31:42 +0530

    implement basic login system WIP

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,13 @@
+import { useState } from 'react';
+import LoginContainer from '../components/LoginContainer';
+
 const Header = () => {
+    const [loginVisible, setLoginVisible] = useState(false);
     return (
-        <div className="h-16 bg-black text-white p-2 w-full">
+        <div className="h-16 bg-black text-white p-2 w-full flex flex-row space-between">
             <h1 className="m-auto text-4xl pl-6 ">Place</h1>
+            <button className="w-24 text-lg text-white border-2 border-slate-900 bg-gray-900 m-0 right-2 top-5 hover:scale-110 mx-6" onClick={() => {setLoginVisible(!loginVisible)}}>Login</button>
+            <LoginContainer loginVisible={loginVisible} setLoginVisible={setLoginVisible}/>
         </div>
     )
 }
---
+++ components/LoginContainer.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useRef } from "react";
+
+const LoginContainer = (props : {loginVisible: boolean, setLoginVisible: React.Dispatch<React.SetStateAction<boolean>>}) => {
+    const {loginVisible, setLoginVisible} = props;
+    const [isLoggedIn, setLogin]  = useState(false);
+    const usernameInput = useRef(null);
+    
+    const loginUser = () => {
+        const usernameElem : HTMLInputElement = usernameInput.current!;
+        const username = usernameElem.value;
+        localStorage.setItem("username", username);
+        setLoginVisible(false);
+        setLogin(true);
+    }
+
+    const logoutUser = () => {
+        localStorage.clear();
+        setLoginVisible(false);
+        setLogin(false);
+    }
+
+    if(!loginVisible){
+        return <div className="absolute hidden"></div>
+    }
+
+
+    return (
+        <div className="w-72 h-96 border-black border-2 rounded-lg flex flex-col p-6 bg-slate-600 text-white m-0 absolute right-4 top-20">
+            {
+                isLoggedIn && (
+                    <div className="flex flex-col justify-around items-center">
+                        <p className="text-xl p-6">Already logged in</p>
+                        <button onClick={logoutUser}>Logout</button>
+                    </div>
+                )
+            }
+            {
+                !isLoggedIn && (
+                    <div className="flex flex-col justify-around">
+                        <h1 className="m-auto text-3xl ">Login</h1>
+                        <label htmlFor="username">Username:</label>
+                        <input name="username" className="border-black focus:outline-none w-auto text-black rounded-sm h-8 py-4 px-2" ref={usernameInput} />
+                        <button className="w-16 text-lg bg-slate-800 border-1 border-slate-800 m-auto my-2" onClick={loginUser}>Login</button>
+                    </div>
+                )
+            }
+        </div>
+    )
+}
+
+export default LoginContainer;
